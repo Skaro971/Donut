16bd5bb3a71cb5d010193a5fb30aae7b9f17db1f
Skaro <willemgironsdp@gmail.com>
2025-11-04 08:56:30 +0100

    [ADD] Exercice 1.2

--- Donut/Donut/Donut.cpp
+++ Donut/Donut/Donut.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <windows.h>
+
+#define CLEAR_CONSOLE "\033c"
+#define RESET_CUROR "\033[H"
+#define HIDE_CURSOR "\033[?25l"
+#define SHOW_CURSOR "\033[?25m"
+
+void ConfigConsole()
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD mode;
+    GetConsoleMode(hConsole, &mode);
+    SetConsoleMode(hConsole, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+}
 
 int main(int argc, char** argv)
 {
+    ConfigConsole();
 
-    std::cout << "Hello World!\n";
+    //std::cout << "Hello World!\n";
+
+    //printf(CLEAR_CONSOLE);
+
+    //std::cout << "Hello World!\n";
+
+    printf(HIDE_CURSOR);
+
+
+
     return 0;
 }
 
